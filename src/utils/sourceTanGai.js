f4d13911656e1da3a53614948b471fcb8e54b1a1
duongxuannam <duongxuannam1995@gmail.com>
2020-08-05 23:13:25 +0700

    puishas

--- src/utils/sourceTanGai.js
+++ src/utils/sourceTanGai.js
@@ -11,6 +11,8 @@ Thương thương <3 <3 <3`,
   `Haydo, một ngày mới bắt đầu. cục mỡ của anh dậy đi học nào.
   Nhớ là dành thời gian nhớ tui nha chưa <3 <3 <3`,
   `Lại một ngày nữa trôi qua. Mặc dù h này chắc anh vẫn ngủ nhưng khá chắc kèo là anh đang mơ về em á. Cục dàng đi học vui vẻ nhoa <3 <3 <3`,
+  `Aida nay lạnh quá trời quá đất luôn ước j đc ôm ai đó mập mập tí ha.
+  Cục dàng yêu dấu đã dậy chưa?? Anh thì chắc dậy rồi á chứ hông có nằm ôm mền ngủ đâu ha mà có làm j thì cũng đang nhớ em lắm luôn <3 <3 <3`,
   `Cho anh ôm em miếng được không?? :'<`,
 ];
 export default source;
